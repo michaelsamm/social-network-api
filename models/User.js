dfa3dbe71d2e8700b126798de10ba2a71fef6f55
msamm <msamm93@gmail.com>
2022-07-02 12:32:27 -0500

    user routes and controller plus add and remove friends

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const UserSchema = new Schema(
         email: {
             type: String,
             unique: true,
-            required: true,
+            required: true
             // validation for matching valid email address
         },
         thoughts: [
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,12 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
 
+// Add '/api' when using the routes in the api directory
+router.use('/api', apiRoutes);
+
+// Return cheeky 404 error text when hitting an unaccounted for destination
+router.use((req, res) => {
+    res.status(404).send('<h1>😝 404 Error!</h1>')
+});
 
 module.exports = router;
